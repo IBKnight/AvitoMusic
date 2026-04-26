745a2b113d3561da9f7e4e2b9b475ccd1d402496
ibknight <youtube.integral@mail.ru>
2026-04-26 17:42:12 +0300

    added CoroutineModule.kt

--- app/src/main/java/com/avito/avitomusic/features/music_player/data/repository/PlayerRepository.kt
+++ app/src/main/java/com/avito/avitomusic/features/music_player/data/repository/PlayerRepository.kt
@@ -1,26 +1,28 @@
 package com.avito.avitomusic.features.music_player.data.repository
 
 import com.avito.avitomusic.common.data.ApiMusicDataSource
+import com.avito.avitomusic.common.di.IoDispatcher
 import com.avito.avitomusic.features.music_list.data.models.TrackModel
 import com.avito.avitomusic.features.music_player.data.models.TrackListResponse
 import com.avito.avitomusic.features.music_player.domain.repository.IPlayerRepository
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-//TODO(IBKnight): надо унести в DI object с диспатчерами
-class PlayerRepository @Inject constructor(private val apiMusicDataSource: ApiMusicDataSource) :
+class PlayerRepository @Inject constructor(
+    private val apiMusicDataSource: ApiMusicDataSource,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+) :
     IPlayerRepository {
-    override suspend fun getTrackList(trackID: Long): TrackListResponse = withContext(dispatcher) {
+    override suspend fun getTrackList(trackID: Long): TrackListResponse = withContext(ioDispatcher) {
         apiMusicDataSource.getTrackList(trackID.toString(), LIMIT.toString())
     }
 
-    override suspend fun getTrack(trackID: Long): TrackModel = withContext(dispatcher) {
+    override suspend fun getTrack(trackID: Long): TrackModel = withContext(ioDispatcher) {
         apiMusicDataSource.getTrack(trackID)
     }
 
-    companion object {
+    private companion object {
         const val LIMIT: Int = 50
-        val dispatcher = Dispatchers.IO
     }
 }
--- app/src/main/java/com/avito/avitomusic/features/saved_music/data/repository/FavouriteTrackRepoImpl.kt
+++ app/src/main/java/com/avito/avitomusic/features/saved_music/data/repository/FavouriteTrackRepoImpl.kt
@@ -1,10 +1,11 @@
 package com.avito.avitomusic.features.saved_music.data.repository
 
 
+import com.avito.avitomusic.common.di.IoDispatcher
 import com.avito.avitomusic.features.saved_music.data.FavouriteTrackDao
 import com.avito.avitomusic.features.saved_music.data.model.FavouriteTrackModel
 import com.avito.avitomusic.features.saved_music.domain.repository.IFavouriteMusicRepo
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
@@ -12,7 +13,8 @@ import javax.inject.Inject
 
 
 class FavouriteTrackRepoImpl @Inject constructor(
-    private val dao: FavouriteTrackDao
+    private val dao: FavouriteTrackDao,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : IFavouriteMusicRepo {
     override suspend fun getSavedTracks(): Flow<List<FavouriteTrackModel>> =
         dao.getAll().map { entities ->
@@ -30,22 +32,22 @@ class FavouriteTrackRepoImpl @Inject constructor(
         }
 
     override suspend fun saveTrack(track: FavouriteTrackModel) =
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             dao.insert(track)
         }
 
     override suspend fun removeTrack(track: FavouriteTrackModel) =
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             dao.delete(track)
         }
 
     override suspend fun isTrackSaved(id: Long): Boolean =
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             dao.isSaved(id)
         }
 
     override suspend fun search(query: String): Flow<List<FavouriteTrackModel>> =
-        withContext(Dispatchers.IO) {
+        withContext(ioDispatcher) {
             dao.search(query)
         }
 }
